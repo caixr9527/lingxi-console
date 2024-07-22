0c82eb538d6839a2c0eaa6fe37906bc93212d263
caixiaorong <smart_r@163.com>
2024-07-22 23:55:13 +0800

    fetch util

---
+++ src/utils/request.ts
@@ -0,0 +1,70 @@
+import { apiPrefix } from '@/config'
+const TIME_OUT = 10 * 1000
+const baseFetchOptions = {
+  method: 'GET',
+  mode: 'cors',
+  credentals: 'include',
+  headers: new Headers({
+    'Content-Type': 'application/json',
+  }),
+  redirect: 'follow',
+}
+
+type FetchOptionType = Omit<RequestInit, 'body'> & {
+  params?: Record<string, any>
+  body?: BodyInit | Record<string, any> | null
+}
+
+const baseFetch = <T>(url: string, fetchOption: FetchOptionType): Promise<T> => {
+  const options: typeof baseFetchOptions & FetchOptionType = Object.assign(
+    {},
+    baseFetchOptions,
+    fetchOption,
+  )
+  let urlWithPrefix = `${apiPrefix}${url.startsWith('/') ? url : `/${url}`}`
+  const { method, params, body } = options
+  if (method === 'GET' && params) {
+    const paramsArray: string[] = []
+    Object.keys(params).forEach((key) => {
+      paramsArray.push(`${key}=${encodeURIComponent(params[key])}`)
+    })
+    if (urlWithPrefix.search(/\?/) === -1) {
+      urlWithPrefix += `?${paramsArray.join('&')}`
+    } else {
+      urlWithPrefix += `&${paramsArray.join('&')}`
+    }
+    delete options.params
+  }
+  if (body) {
+    options.body = JSON.stringify(body)
+  }
+  return Promise.race([
+    new Promise((_resolve, reject) => {
+      setTimeout(() => {
+        reject('接口超时')
+      }, TIME_OUT)
+    }),
+    new Promise((resolve, reject) => {
+      globalThis
+        .fetch(urlWithPrefix, options as RequestInit)
+        .then((res) => {
+          resolve(res.json)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    }),
+  ]) as Promise<T>
+}
+
+export const request = <T>(url: string, options = {}) => {
+  return baseFetch<T>(url, options)
+}
+
+export const get = <T>(url: string, options = {}) => {
+  return request<T>(url, Object.assign({}, options, { method: 'GET' }))
+}
+
+export const post = <T>(url: string, options = {}) => {
+  return request<T>(url, Object.assign({}, options, { method: 'POST' }))
+}
---
+++ src/views/space/apps/DetailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-button @click="debug">发起请求</a-button>
+</template>
+
+<script setup lang="ts">
+import { post } from '@/utils/request'
+const debug = async () => {
+  const response = await post('/apps/7a1131d6-722d-43cf-94db-df23082bccc0/debug', {
+    body: {
+      query: '你好，讲一个关于程序员的冷笑话',
+    },
+  })
+  console.log(response)
+}
+</script>
+
+<style></style>
