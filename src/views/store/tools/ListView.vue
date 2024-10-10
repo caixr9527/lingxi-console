dba9ff73924a85266df1745cec94b3725088e98f
caixiaorong <smart_r@163.com>
2024-10-10 22:39:43 +0800

    利用抽屉展示内置提供者详情

--- src/views/store/tools/ListView.vue
+++ src/views/store/tools/ListView.vue
@@ -10,6 +10,7 @@ const providers = reactive<Array<any>>([])
 const loading = ref<boolean>(false)
 const category = ref<string>('all')
 const search_word = ref<string>('')
+const showIndex = ref<number>(-1)
 const filterProviders = computed(() => {
   return providers.filter((item) => {
     const matchCategory = category.value === 'all' || item.category === category.value
@@ -77,8 +78,8 @@ onMounted(async () => {
       <!-- 底部插件列表 -->
       <a-row :gutter="[20, 20]" class="flex-1">
         <!-- 有数据 -->
-        <a-col v-for="provider in filterProviders" :key="provider.name" :span="6">
-          <a-card hoverable class="cursor-pointer rounded-lg">
+        <a-col v-for="(provider, idx) in filterProviders" :key="provider.name" :span="6">
+          <a-card hoverable class="cursor-pointer rounded-lg" @click="showIndex = idx">
             <!-- 顶部提供商名称 -->
             <div class="flex items-center gap-3 mb-3">
               <!-- 左侧图标 -->
@@ -119,6 +120,86 @@ onMounted(async () => {
           />
         </a-col>
       </a-row>
+      <!-- 卡片抽屉 -->
+      <a-drawer
+        :visible="showIndex != -1"
+        :width="350"
+        :footer="false"
+        title="工具详情"
+        :drawer-style="{ background: '#F9FAFB' }"
+        @cancel="showIndex = -1"
+      >
+        <div v-if="showIndex !== -1">
+          <!-- 顶部提供商名称 -->
+          <div class="flex items-center gap-3 mb-3">
+            <!-- 左侧图标 -->
+            <a-avatar
+              :size="40"
+              shape="square"
+              :style="{ backgroundColor: filterProviders[showIndex].background }"
+            >
+              <img
+                :src="`${apiPrefix}/builtin-tools/${filterProviders[showIndex].name}/icon`"
+                :alt="filterProviders[showIndex].name"
+              />
+            </a-avatar>
+            <!-- 右侧工具信息 -->
+            <div class="flex flex-col">
+              <div class="text-base text-gray-900 font-bold">
+                {{ filterProviders[showIndex].label }}
+              </div>
+              <div class="text-xs text-gray-500 line-clamp-1">
+                {{ filterProviders[showIndex].name }} ▪️
+                {{ filterProviders[showIndex].tools.length }} 插件
+              </div>
+            </div>
+          </div>
+          <!-- 提供商描述信息 -->
+          <div class="leading-[18px] text-gray-500 mb-2">
+            {{ filterProviders[showIndex].description }}
+          </div>
+          <!-- 分隔符 -->
+          <hr class="my-4" />
+          <!-- 提供者工具列表 -->
+          <div class="flex flex-col">
+            <div class="mb-3 text-xs text-gray-500">
+              包含 {{ filterProviders[showIndex].tools.length }} 个工具
+            </div>
+            <!-- 工具列表 -->
+            <a-card
+              v-for="tool in filterProviders[showIndex].tools"
+              :key="tool.name"
+              class="cursor-pointer flex flex-col rounded-xl"
+            >
+              <!-- 工具名称 -->
+              <div class="font-bold text-gray-900 mb-2">{{ tool.label }}</div>
+              <!-- 工具描述 -->
+              <div class="text-gray-500 text-xs">{{ tool.description }}</div>
+              <!-- 工具参数 -->
+              <div v-if="tool.inputs.length > 0" class="">
+                <!-- 分隔符 -->
+                <div class="flex items-center gap-2 my-4">
+                  <div class="text-xs font-bold text-gray-500">参数</div>
+                  <hr class="flex-1" />
+                </div>
+                <!-- 参数列表 -->
+                <div class="flex flex-col gap-4">
+                  <div v-for="input in tool.inputs" :key="input.name" class="flex flex-col gap-2">
+                    <!-- 上半部分 -->
+                    <div class="flex items-center gap-2 text-xs">
+                      <div class="text-gray-900 font-bold">{{ input.name }}</div>
+                      <div class="text-gray-500">{{ input.type }}</div>
+                      <div v-if="input.required" class="text-red-700">必填</div>
+                    </div>
+                    <!-- 下半部分 -->
+                    <div class="text-xs text-gary-500">{{ input.description }}</div>
+                  </div>
+                </div>
+              </div>
+            </a-card>
+          </div>
+        </div>
+      </a-drawer>
     </div>
   </a-spin>
 </template>
