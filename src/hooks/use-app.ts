afc204d7b6780bf476d492c1ed72a1803993c4f8
caixiaorong <smart_r@163.com>
2024-12-12 22:29:41 +0800

    发布历史页面与回退

--- src/hooks/use-app.ts
+++ src/hooks/use-app.ts
@@ -1,4 +1,10 @@
-import { cancelPublish, getApp, publish } from '@/services/app'
+import {
+  cancelPublish,
+  fallbackHistoryToDraft,
+  getApp,
+  getPublishHistoriesWithPage,
+  publish,
+} from '@/services/app'
 import { Message, Modal } from '@arco-design/web-vue'
 import { onMounted, reactive, ref } from 'vue'
 
@@ -59,4 +65,69 @@ export const useCancelPublish = () => {
   }
 
   return { loading, handleCancelPublish }
+}
+
+export const useGetPublishHistoriesWithPage = () => {
+  const loading = ref(false)
+  const defaultPaginator = {
+    current_page: 1,
+    page_size: 10,
+    total_page: 0,
+    total_record: 0,
+  }
+  const publishHistories = reactive<Array<Record<string, any>>>([])
+  const paginator = reactive({ ...defaultPaginator })
+
+  const loadPublishHistories = async (app_id: string, init: boolean = false) => {
+    try {
+      if (init) {
+        Object.assign(paginator, { ...defaultPaginator })
+      } else if (paginator.current_page > paginator.total_page) {
+        return
+      }
+      loading.value = true
+      const resp = await getPublishHistoriesWithPage(app_id, {
+        current_page: paginator.current_page,
+        page_size: paginator.page_size,
+      })
+      const data = resp.data
+      paginator.current_page = data.paginator.current_page
+      paginator.page_size = data.paginator.page_size
+      paginator.total_page = data.paginator.total_page
+      paginator.total_record = data.paginator.total_record
+
+      if (paginator.current_page <= paginator.total_page) {
+        paginator.current_page += 1
+      }
+
+      if (init) {
+        publishHistories.splice(0, publishHistories.length, ...data.list)
+      } else {
+        publishHistories.push(...data.list)
+      }
+    } finally {
+      loading.value = false
+    }
+  }
+  return { loading, publishHistories, paginator, loadPublishHistories }
+}
+
+export const useFallbackHistoryToDraft = () => {
+  const loading = ref(false)
+
+  const handleFallbackHistoryToDraft = async (
+    app_id: string,
+    app_config_version_id: string,
+    callback?: () => void,
+  ) => {
+    try {
+      loading.value = true
+      const resp = await fallbackHistoryToDraft(app_id, app_config_version_id)
+      Message.success(resp.message)
+    } finally {
+      loading.value = false
+      callback && callback()
+    }
+  }
+  return { loading, handleFallbackHistoryToDraft }
 }
