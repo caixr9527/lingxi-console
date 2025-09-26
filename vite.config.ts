10a9f161ec9fa281b926826784812d8193c70e59
caixiaorong <smart_r@163.com>
2025-09-26 20:29:42 +0800

    添加@originjs/vite-plugin-commonjs依赖

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCommonjs({
+      include: ['jsoneditor'],
+    }),
+  ],
   build: {
     rollupOptions: {
       external: ['jsoneditor'], // 明确声明为外部依赖
